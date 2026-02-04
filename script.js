74a6ccea46de2a70c23946617e6291f23e225de1
beingzenitsu <ojasvchaudhary0@gmail.com>
2026-02-04 15:33:37 +0530

    real jpg photos

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ const snowballDiv = document.getElementById("snowball");
 const question = document.getElementById("question");
 const music = document.getElementById("bgMusic");
 const typeText = document.getElementById("typeText");
+const slideshow = document.getElementById("slideshow");
+const slideImg = document.getElementById("slideImg");
 
-// ❤️ Typewriter message
 const message = `Lavanya Singh (Chinoooooo) ❤️
 
 From the day you came into my life,
@@ -33,7 +34,6 @@ function typeWriter() {
 
 window.onload = typeWriter;
 
-// Make NO button run away
 noBtn.addEventListener("mouseover", () => {
     const x = Math.random() * (window.innerWidth - 100);
     const y = Math.random() * (window.innerHeight - 50);
@@ -43,7 +43,34 @@ noBtn.addEventListener("mouseover", () => {
     noBtn.style.top = `${y}px`;
 });
 
-// YES button click
+
+const photos = [
+  "photo1.jpg","photo2.jpg","photo3.jpg","photo4.jpg","photo5.jpg",
+  "photo6.jpg","photo7.jpg","photo8.jpg","photo9.jpg","photo10.jpg",
+  "photo11.jpg","photo12.jpg","photo13.jpg","photo14.jpg",
+  "photo15.jpg","photo16.jpg","photo17.jpg"
+];
+
+let photoIndex = 0;
+
+function startSlideshow() {
+    slideshow.style.display = "flex";
+
+    slideImg.src = photos[photoIndex];
+
+    setInterval(() => {
+        slideImg.style.opacity = "0";
+
+        setTimeout(() => {
+            photoIndex = (photoIndex + 1) % photos.length;
+            slideImg.src = photos[photoIndex];
+            slideImg.style.opacity = "1";
+        }, 800);
+
+    }, 3500);
+}
+
+
 yesBtn.addEventListener("click", () => {
     question.innerText = "";
     yesBtn.style.display = "none";
@@ -51,4 +78,8 @@ yesBtn.addEventListener("click", () => {
     typeText.style.display = "none";
     snowballDiv.classList.remove("hidden");
     music.play();
+
+    setTimeout(() => {
+    startSlideshow();
+}, 1500);
 });
